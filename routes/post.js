9bfd81f7f6aaf9a1ca80e75ef769723c793058b9
최혜원Vicky <fongfing@uneedcomms.com>
2021-07-17 23:59:38 +0900

    feat: post routes 내 이미지 업로드 이벤트를 s3에 연결

--- routes/post.js
+++ routes/post.js
@@ -1,40 +1,47 @@
-﻿const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+﻿const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const AWS = require("aws-sdk");
+const multerS3 = require("multer-s3");
 
-const { Post, Hashtag } = require('../models');
-const { isLoggedIn } = require('./middlewares');
+const { Post, Hashtag } = require("../models");
+const { isLoggedIn } = require("./middlewares");
 
 const router = express.Router();
 
 try {
-  fs.readdirSync('uploads');
+  fs.readdirSync("uploads");
 } catch (error) {
-  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-  fs.mkdirSync('uploads');
+  console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다.");
+  fs.mkdirSync("uploads");
 }
 
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: "ap-northeast-2",
+});
+
 const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, cb) {
-      cb(null, 'uploads/');
-    },
-    filename(req, file, cb) {
-      const ext = path.extname(file.originalname);
-      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+  storage: multerS3({
+    s3: new AWS.S3(),
+    bucket: "nodebird-vicky",
+    key(req, file, cb) {
+      // original 폴더에 저장하며, 같은 이름일 경우 덮어쓰기 방지를 위해 Date.now() 추가
+      cb(null, `original/${Date.now()}${path.basename(file.originalname)}`);
     },
   }),
   limits: { fileSize: 5 * 1024 * 1024 },
 });
 
-router.post('/img', isLoggedIn, upload.single('img'), (req, res) => {
+router.post("/img", isLoggedIn, upload.single("img"), (req, res) => {
   console.log(req.file);
-  res.json({ url: `/img/${req.file.filename}` });
+  res.json({ url: req.file.location });
 });
 
 const upload2 = multer();
-router.post('/', isLoggedIn, upload2.none(), async (req, res, next) => {
+router.post("/", isLoggedIn, upload2.none(), async (req, res, next) => {
   try {
     console.log(req.user);
     const post = await Post.create({
@@ -45,15 +52,15 @@ router.post('/', isLoggedIn, upload2.none(), async (req, res, next) => {
     const hashtags = req.body.content.match(/#[^\s#]*/g);
     if (hashtags) {
       const result = await Promise.all(
-        hashtags.map(tag => {
+        hashtags.map((tag) => {
           return Hashtag.findOrCreate({
             where: { title: tag.slice(1).toLowerCase() },
-          })
-        }),
+          });
+        })
       );
-      await post.addHashtags(result.map(r => r[0]));
+      await post.addHashtags(result.map((r) => r[0]));
     }
-    res.redirect('/');
+    res.redirect("/");
   } catch (error) {
     console.error(error);
     next(error);
